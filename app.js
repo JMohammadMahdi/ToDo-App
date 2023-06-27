bd17d53295531b2200637c77815eaca54a4a7685
Mohammad Mahdi Jalali <m.m.jalali84@mail.com>
2023-06-27 15:34:20 +0330

    Adding changes to the frontend and saving the status of the task in the JS file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", showTodos)
 // keys Handle
 document.addEventListener("keydown", keyHandler)
 
-// elements
+// elements list
 const ListItems = document.getElementById("list-items");
 
 addTodoButton.addEventListener("click", newTodoHadeler);
@@ -17,26 +17,36 @@ function newTodoHadeler(event) {
         addTodoInput.value = null;
 
         isLoad = false;
-        CreateToDo(Taskdetails);
+        CreateToDo(Taskdetails, "unfinished");
     } else {
-        alert("inter something!")
+        alert("Please write something!");
     }
 }
 
 let IsLoad = false;
-function CreateToDo(Taskdetails) {
+function CreateToDo(Taskdetails, status) {
     // create element
     const element = document.createElement("li");
     const TaskTitle = document.createElement("h2");
     const TaskStatus = document.createElement("img");
     const DeleteTask = document.createElement("img")
 
-    // set values: 
+    // set todo values: 
     TaskTitle.innerText = Taskdetails;
-    // set attribute
-    TaskStatus.setAttribute("src", "./SVG/notAccepted.svg");
-    TaskStatus.setAttribute("alt", "notAccept");
-    TaskStatus.setAttribute("id", "uncompleted")
+    // set style for todo item
+    if (status == "completed") {
+        // set attribute
+        TaskStatus.setAttribute("src", "./SVG/Accepted.svg");
+        TaskStatus.setAttribute("id", "completed");
+        element.classList.add("completed");
+    } else if (status == "unfinished") {
+        // set attribute
+        TaskStatus.setAttribute("src", "./SVG/notAccepted.svg");
+        TaskStatus.setAttribute("id", "unfinished");
+        element.classList.remove("completed");
+    }
+
+    // TaskStatus.setAttribute("id", "unfinished");
     TaskStatus.classList.add("status");
     DeleteTask.setAttribute("src", "./SVG/Close.svg");
     DeleteTask.setAttribute("alt", "delete task");
@@ -55,27 +65,35 @@ function CreateToDo(Taskdetails) {
 }
 
 function AddTaskToLocalStorage(TasksValue) {
-    let GetTask = JSON.parse(localStorage.getItem("task"));
+    let GetTask = JSON.parse(localStorage.getItem("ToDos"));
     (GetTask == null) ? GetTask = [] : null;
-    GetTask.push(TasksValue);
+
+    const TODO = {
+        title: TasksValue,
+        status: "unfinished"
+    };
+    GetTask.push(TODO);
 
     // Push to Localstorage
-    localStorage.setItem("task", JSON.stringify(GetTask));
+    localStorage.setItem("ToDos", JSON.stringify(GetTask));
 }
 
 function DeleteTaskFromLocalStorage(TasksValue) {
-    let GetTask = JSON.parse(localStorage.getItem("task"));
-    const index = GetTask.indexOf(TasksValue);
+    let GetTask = JSON.parse(localStorage.getItem("ToDos"));
 
-    GetTask.splice(index, 1);
+    GetTask.forEach((todo, index) => {
+        if (todo['title'] == TasksValue) {
+            GetTask.splice(index, 1);
+        }
+    });
 
     // push to local storage
-    localStorage.setItem("task", JSON.stringify(GetTask));
+    localStorage.setItem("ToDos", JSON.stringify(GetTask));
 }
 
-// delete & compelet handler
+// delete & status element handler
 ListItems.addEventListener("click", event => {
-    item = event.target;
+    const item = event.target;
 
     if (item.classList[0] == "delete") {
         itemValue = item.previousElementSibling.innerText;
@@ -86,28 +104,53 @@ ListItems.addEventListener("click", event => {
         DeleteTaskFromLocalStorage(itemValue);
     }
     else if (item.classList[0] == "status") {
+        let GetTask = JSON.parse(localStorage.getItem("ToDos"));
+
         const parent = item.parentElement;
         const status = item.id;
 
-        if (status == "uncompleted") {
+        if (status == "unfinished") {
             item.setAttribute("src", "./SVG/Accepted.svg");
             item.setAttribute("id", "completed");
-            parent.classList.add("completed")
+            parent.classList.add("completed");
+
+            changeStatus(item, "completed")
         } else if (status == "completed") {
             item.setAttribute("src", "./SVG/notAccepted.svg");
-            item.setAttribute("id", "uncompleted");
+            item.setAttribute("id", "unfinished");
             parent.classList.remove("completed");
+
+            changeStatus(item, "unfinished");
         }
+    }
+
+    function changeStatus(element, status) {
+        const GetTask = JSON.parse(localStorage.getItem("ToDos"));
+        const title = element.nextElementSibling.innerText;
+
+        // change status from Local Storage
+        GetTask.forEach(todo => {
+            if (todo['title'] == title) {
+                todo['status'] = status;
+            }
+        });
+
+        // push to local storage
+        localStorage.setItem("ToDos", JSON.stringify(GetTask));
     }
 });
 
 function showTodos() {
-    const GetTask = JSON.parse(localStorage.getItem("task"));
+    const GetTask = JSON.parse(localStorage.getItem("ToDos"));
 
     isLoad = true;
-    for (const iterator of GetTask) {
-        CreateToDo(iterator);
-    }
+    GetTask.forEach(todo => {
+        const title = todo['title'];
+        const status = todo['status'];
+
+        // console.log(`on show todo: ${title}, ${status}`);
+        CreateToDo(title, status);
+    });
 }
 
 // key Handler
@@ -116,7 +159,7 @@ function keyHandler(event) {
     if (key == "Enter") {
         if (addTodoInput.value) {
             isLoad = false;
-            CreateToDo(addTodoInput.value);
+            CreateToDo(addTodoInput.value, "unfinished");
             addTodoInput.value = null;
         } else {
             alert("inter something!")
